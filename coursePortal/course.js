99ca3264c413a3537b1659a7882764b0e779f1e0
Lakeram Narine <nar19002@byui.edu>
2021-01-08 21:35:16 -0700

    change

--- coursePortal/course.js
+++ coursePortal/course.js
@@ -1,3 +1,5 @@
 var string = document.lastModified;
-
+let year = new Date
+year = year.getFullYear()
 document.getElementById('datemodified').textContent = "Last Updated: " + string
+document.getElementById("currentYear").textContent = year
